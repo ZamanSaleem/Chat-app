5190a1541295b2ff99029ab91981d6ef20c79e7b
ZamanSaleem <137734806+ZamanSaleem@users.noreply.github.com>
2025-04-17 23:40:49 +0500

    Update util.js

--- frontend/src/lib/util.js
+++ frontend/src/lib/util.js
@@ -1,1 +1,7 @@
-file added 
+export function formatMessageTime(date) {
+  return new Date(date).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+}
